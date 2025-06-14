9d2188a2d14bc704bb0c09a94729e2f5e1a8e828
ImStyrene <ImStyrene@protonmail.com>
2025-06-13 23:03:41 -0400

    Update harpoon.lua

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,2 +1,16 @@
 if true then return {} end -- WARN: REMOVE THIS LINE TO ENABLE THIS FILE
 
+local ui = require("harpoon.ui")
+local mark = require("harpoon.mark")
+
+return {
+  "ThePrimeagen/harpoon"
+  config = {
+    keys - {
+      -- Press XX... key/s to open the Harpoon UI
+      vim.keymap.set("n", "<leader>AU", vim.cmd.ui.toggle_quick_menu()
+      -- Press XX... key/s to mark a file
+      vim.keymap.set"n", "<leader>MF", vim.cmd.mark.add_file()
+    }
+  },
+}
