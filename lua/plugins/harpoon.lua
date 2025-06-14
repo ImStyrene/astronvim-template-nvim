ee6f5a1d0f68bc0086547d4ebae034c1aa27e4b9
ImStyrene <ImStyrene@protonmail.com>
2025-06-13 23:13:46 -0400

    Update harpoon.lua

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,9 +8,9 @@ return {
   config = {
     keys - {
       -- Press XX... key/s to open the Harpoon UI
-      vim.keymap.set("n", "<leader>AU", vim.cmd.ui.toggle_quick_menu()
+      vim.keymap.set("n", "<leader>AU", vim.cmd.ui.toggle_quick_menu(),
       -- Press XX... key/s to mark a file
-      vim.keymap.set"n", "<leader>MF", vim.cmd.mark.add_file()
+      vim.keymap.set"n", "<leader>MF", vim.cmd.mark.add_file(),
     }
   },
 }
